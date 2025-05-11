1c0dfb28ba2c95293501e564562677f754f9689e
sheetalShishodia <105702145+sheetalShishodia@users.noreply.github.com>
2025-05-11 17:39:56 +0530

    connection_change

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use(express.json());
 app.use('/api/sessions', sessionRoutes);
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
